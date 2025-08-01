38bef5e12fc5710c827735ceb9fdec9a714d1e54
STANSLOUS <209274992+MSTANSLOUS@users.noreply.github.com>
2025-08-01 17:32:13 +0200

    Update __init__.py

--- my_simple_math/__init__.py
+++ my_simple_math/__init__.py
@@ -1,5 +1,22 @@
-from .operations import add, subtract
+from .operations import (
+    add,
+    subtract,
+    multiply,
+    divide,
+    power,
+    factorial,
+    is_prime
+)
+
 
 __version__ = '0.1.0'
-__all__ = ['add', 'subtract']
+__all__ = [
+    'add',
+    'subtract',
+    'multiply',
+    'divide',
+    'power',
+    'factorial',
+    'is_prime'
+]
 
